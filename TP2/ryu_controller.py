3b776841789ea0b2c2bcda9947711515fd39d5db
Maria João Moreira <61200595+mariajmoreira@users.noreply.github.com>
2022-05-06 18:40:24 +0100

    DONE

--- TP2/ryu_controller.py
+++ TP2/ryu_controller.py
@@ -3,41 +3,46 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.ofproto import ofproto_parser
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from ryu.lib.packet import arp
 from ryu.lib.packet import ipv4
-from ryu.lib.packet import icmp,arp
+from ryu.lib.packet import icmp
+from ryu.ofproto import ether
 
-
-
-class SimpleSwitch13(app_manager.RyuApp):
+class Exercicio2(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
-        # initialize mac address table.
+        super(Exercicio2, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.L3SwitchMACs = {}
+        self.ipToPort = {'10.0.1.254': 1 ,'10.0.2.245': 2,'10.0.3.254': 3}
+        self.portToIP= {1: '10.0.1.254', 2: '10.0.2.254', 3:'10.0.3.254'}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        msg = ev.msg
-        datapath = msg.datapath
+        datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        
         # install table-miss flow entry
         #
         # We specify NO BUFFER to max_len of the output action due to
         # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
         # truncated packet data. In that case, we cannot output packets
         # correctly.  The bug has been fixed in OVS v2.1.0.
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+
+        match = parser.OFPMatch(eth_type = ether_types.ETH_TYPE_IPV6)
+        actions = []
+        self.add_flow(datapath, 1, match, actions)
+        self.send_port_desc_stats_request(datapath)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
@@ -54,37 +59,74 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def send_port_desc_stats_request(self, datapath):
+        ofp_parser = datapath.ofproto_parser
+
+        req = ofp_parser.OFPPortDescStatsRequest(datapath, 0)
+        datapath.send_msg(req)
+        
     def _handle_arp(self, datapath, port, pkt_ethernet, pkt_arp):
         if pkt_arp.opcode != arp.ARP_REQUEST:
             return
+        
         pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
-                                           dst=pkt_ethernet.src,
-                                           src=self.hw_addr))
-        pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
-                                 src_mac=self.hw_addr,
-                                 src_ip=self.ip_addr,
-                                 dst_mac=pkt_arp.src_mac,
-                                 dst_ip=pkt_arp.src_ip))
+        
+        for key,value in self.L3SwitchMACs.items():
+            self.logger.info('key: %s , value: %s, port: %s ', key, value, port)
+            #if port in self.L3SwitchMACs.keys():
+            if port==key:    
+                self.logger.info('port: %s == key dic: %s ', port, key)
+                self.logger.info('MAC address: %s', value)
+                
+                for k,v in self.portToIP.items():
+                    if port == k:
+                        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
+                                                   dst=pkt_ethernet.src,
+                                                   src=value))
+                        pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
+                                         src_mac=value,
+                                         src_ip=v,
+                                         dst_mac=pkt_arp.src_mac,
+                                         dst_ip=pkt_arp.src_ip))
+            else:
+                pass 
+                                   
         self._send_packet(datapath, port, pkt)
+        self.logger.info("ARP-Reply sent")
+     
 
     def _handle_icmp(self, datapath, port, pkt_ethernet, pkt_ipv4, pkt_icmp):
         if pkt_icmp.type != icmp.ICMP_ECHO_REQUEST:
             return
         pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
-                                           dst=pkt_ethernet.src,
-                                           src=self.hw_addr))
-        pkt.add_protocol(ipv4.ipv4(dst=pkt_ipv4.src,
-                                   src=self.ip_addr,
-                                   proto=pkt_ipv4.proto))
-        pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY,
-                                   code=icmp.ICMP_ECHO_REPLY_CODE,
-                                   csum=0,
-                                   data=pkt_icmp.data))
-        self._send_packet(datapath, port, pkt)
-    
+        
+        
+        for key,value in self.L3SwitchMACs.items():
+            self.logger.info('key: %s , value: %s, port: %s ', key, value, port)
+            #if port in self.L3SwitchMACs.keys():
+            if port==key:    
+                self.logger.info('port: %s == key dic: %s ', port, key)
+                self.logger.info('MAC address: %s', value)
+                
+                for k,v in self.portToIP.items():
+                    if port == k:
+                        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
+                                                           dst=pkt_ethernet.src,
+                                                           src=value))
+
+                        pkt.add_protocol(ipv4.ipv4(dst=pkt_ipv4.src,
+                                                   src=v,
+                                                   proto=pkt_ipv4.proto))
+
+                        pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY,
+                                                   code=icmp.ICMP_ECHO_REPLY_CODE,
+                                                   csum=0,
+                                                   data=pkt_icmp.data))
+        
+        self._send_packet(datapath, port, pkt)    
+        
+            
+   
     def _send_packet(self, datapath, port, pkt):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -98,7 +140,17 @@ class SimpleSwitch13(app_manager.RyuApp):
                                   actions=actions,
                                   data=data)
         datapath.send_msg(out)
-    
+        
+
+    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
+    def port_desc_stats_reply_handler(self, ev):
+        for p in ev.msg.body:
+            self.L3SwitchMACs.update({p.port_no : p.hw_addr})
+            self.logger.info('Dicionario de MACS: %s %s', p.port_no, p.hw_addr)
+        #print(self.L3SwitchMACs) 
+        self.logger.info('Dicionario de MACS: %s', self.L3SwitchMACs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
@@ -113,14 +165,34 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        
+        self.logger.info("packet-in %s" % (pkt,))
+        pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
+        if not pkt_ethernet:
+            return
+        
+        pkt_arp = pkt.get_protocol(arp.arp)
+        if pkt_arp:
+            self.logger.info("ARP-Request sent")
+            self._handle_arp(datapath, in_port, pkt_ethernet, pkt_arp)
+            return
+        
+        pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
+        pkt_icmp = pkt.get_protocol(icmp.icmp)
+        if pkt_icmp:
+            self._handle_icmp(datapath, in_port, pkt_ethernet, pkt_ipv4, pkt_icmp)
+            return
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
+        if eth.ethertype == ether_types.ETH_TYPE_IPV6:
+            # ignore ipv6
+            return
         dst = eth.dst
         src = eth.src
 
-        dpid = datapath.id
+        dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
@@ -135,25 +207,18 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port)]
 
+        """
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-
-            # check IP Protocol and create a match for IP
-            if eth.ethertype == ether_types.ETH_TYPE_IP:
-                ip = pkt.get_protocol(ipv4.ipv4)
-                srcip = ip.src
-                dstip = ip.dst
-                match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                        ipv4_src=srcip,
-                                        ipv4_dst=dstip
-                                        )
-                # verify if we have a valid buffer_id, if yes avoid to send both
-                # flow_mod & packet_out
-                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                    return
-                else:
-                    self.add_flow(datapath, 1, match, actions)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        """
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
@@ -162,25 +227,4 @@ class SimpleSwitch13(app_manager.RyuApp):
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
         
-    @set_ev_cls(ofp_event.EventOFPPacketIn,CONFIG_DISPATCHER)    
-    def send_port_desc_stats_request(self, ev):
-        msg = ev.msg
-        datapath = msg.datapath
-        ofp_parser = datapath.ofproto_parser
-
-        req = ofp_parser.OFPPortDescStatsRequest(datapath, 0)
-        datapath.send_msg(req)
-        
-    def port_desc_stats_reply_handler(self, ev):
-        ports = []
-        for p in ev.msg.body:
-            ports.append('port_no=%d hw_addr=%s name=%s config=0x%08x '
-                         'state=0x%08x curr=0x%08x advertised=0x%08x '
-                         'supported=0x%08x peer=0x%08x curr_speed=%d '
-                         'max_speed=%d' %
-                         (p.port_no, p.hw_addr,
-                          p.name, p.config,
-                          p.state, p.curr, p.advertised,
-                          p.supported, p.peer, p.curr_speed,
-                          p.max_speed))
-        self.logger.debug('OFPPortDescStatsReply received: %s', ports)    
+    
--- TP2/topologia_tp2.py
+++ TP2/topologia_tp2.py
@@ -17,79 +17,67 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 """
 from cgi import test
 import time
-from mininet.net import Mininet
+from mininet.net import Mininet, VERSION
 from mininet.node import RemoteController  # CPULimitedHost
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.topo import Topo
+from asyncio import protocols
+from mininet.node import OVSSwitch
 
-class MyTopo( Topo ):
-    "Exercise 2 topology"
-
-    def build( self ):
-        "Create custom topology"
-
-        # Add hosts and switches
-
-        switchL3 = self.addSwitch( 'L3', ip='10.0.0.1' )
-        
-        switchL2_A = self.addSwitch('L2A', ip='10.0.1.1' , defaultRoute='via 10.0.0.1')
-        host1_A = self.addHost('h1A', ip='10.0.1.2' , defaultRoute=' via 10.0.1.1')
-        host2_A= self.addHost('h2A', ip='10.0.1.3' , defaultRoute='via 10.0.1.1')
-        host3_A = self.addHost('h3A', ip='10.0.1.4' , defaultRoute='via 10.0.1.1')
-        host4_A = self.addHost('h4A', ip='10.0.1.5' , defaultRoute='via 10.0.1.1')
-        
-        switchL2_B = self.addSwitch('L2B' ,ip='10.0.2.1' , defaultRoute='via 10.0.0.1')
-        host1_B = self.addHost('h1B', ip='10.0.2.2' , defaultRoute='via 10.0.2.1')
-        host2_B= self.addHost('h2B', ip='10.0.2.3', defaultRoute='via 10.0.2.1')
-        host3_B = self.addHost('h3B', ip='10.0.2.4', defaultRoute='via 10.0.2.1')
-        host4_B = self.addHost('h4B', ip='10.0.2.5', defaultRoute='via 10.0.2.1')
-        
-        switchL2_C = self.addSwitch('L2C', ip='10.0.3.1' , defaultRoute='via 10.0.0.1')
-        host1_C = self.addHost('h1C', ip='10.0.3.2', defaultRoute='via 10.0.3.1')
-        host2_C= self.addHost('h2C', ip='10.0.3.3', defaultRoute='via 10.0.3.1')
-        host3_C = self.addHost('h3C', ip='10.0.3.4', defaultRoute='via 10.0.3.1')
-        host4_C = self.addHost('h4C', ip='10.0.3.5', defaultRoute='via 10.0.3.1')
-
-
-        # Add links
-        self.addLink( switchL3, switchL2_A, params2={'ip': '10.0.1.1/24'})
-        self.addLink( switchL3, switchL2_B,   params2={'ip': '10.0.2.1/24'})
-        self.addLink( switchL3, switchL2_C,params2={'ip': '10.0.3.1/24'})
-        
-        self.addLink( switchL2_A, host1_A)
-        self.addLink( switchL2_A, host2_A)
-        self.addLink( switchL2_A, host3_A)
-        self.addLink( switchL2_A, host4_A)
-        
-        self.addLink( switchL2_B, host1_B)
-        self.addLink( switchL2_B, host2_B)
-        self.addLink( switchL2_B, host3_B)
-        self.addLink( switchL2_B, host4_B)
-        
-        self.addLink( switchL2_C, host1_C)
-        self.addLink( switchL2_C, host2_C)
-        self.addLink( switchL2_C, host3_C)
-        self.addLink( switchL2_C, host4_C)
-
-topos = { 'mytopo': ( lambda: MyTopo() ) }
-
-
-def test_RemoteController():
-  topo = MyTopo()
-  print("***TOPO created")
-  net = Mininet(topo=topo, controller=None)
-  net.addController('c0', controller=RemoteController, ip='127.0.0.1')
+def topo():
+  "Create custom topology"
+  # Add hosts and switches
+  net = Mininet(controller=RemoteController, switch=OVSSwitch)
+  switchL3 = net.addSwitch( 'L3', protocols='OpenFlow13')
+  
+  switchL2_A = net.addSwitch('L2A', protocols='OpenFlow13')
+  host1_A = net.addHost('h1A', ip='10.0.1.1/24' ,mac='00:00:00:00:00:01', defaultRoute=' via 10.0.1.254')
+  host2_A= net.addHost('h2A', ip='10.0.1.2/24' ,mac='00:00:00:00:00:02', defaultRoute='via 10.0.1.254')
+  host3_A = net.addHost('h3A', ip='10.0.1.3/24' ,mac='00:00:00:00:00:03', defaultRoute='via 10.0.1.254')
+  host4_A = net.addHost('h4A', ip='10.0.1.4/24' ,mac='00:00:00:00:00:04', defaultRoute='via 10.0.1.254')
+  
+  switchL2_B = net.addSwitch('L2B', protocols='OpenFlow13')
+  host1_B = net.addHost('h1B', ip='10.0.2.1/24',mac='00:00:00:00:00:05' , defaultRoute='via 10.0.2.254')
+  host2_B= net.addHost('h2B', ip='10.0.2.2/24',mac='00:00:00:00:00:06', defaultRoute='via 10.0.2.254')
+  host3_B = net.addHost('h3B', ip='10.0.2.3/24',mac='00:00:00:00:00:07', defaultRoute='via 10.0.2.254')
+  host4_B = net.addHost('h4B', ip='10.0.2.4/24',mac='00:00:00:00:00:08', defaultRoute='via 10.0.2.254')
+  
+  switchL2_C = net.addSwitch('L2C', protocols='OpenFlow13')
+  host1_C = net.addHost('h1C', ip='10.0.3.1/24',mac='00:00:00:00:00:09', defaultRoute='via 10.0.3.254')
+  host2_C= net.addHost('h2C', ip='10.0.3.2/24',mac='00:00:00:00:00:10', defaultRoute='via 10.0.3.254')
+  host3_C = net.addHost('h3C', ip='10.0.3.3/24',mac='00:00:00:00:00:11', defaultRoute='via 10.0.3.254')
+  host4_C = net.addHost('h4C', ip='10.0.3.4/24',mac='00:00:00:00:00:12', defaultRoute='via 10.0.3.254')
+  # Add links
+  net.addLink( switchL3, switchL2_A)
+  net.addLink( switchL3, switchL2_B)
+  net.addLink( switchL3, switchL2_C)
+  
+  net.addLink( switchL2_A, host1_A)
+  net.addLink( switchL2_A, host2_A)
+  net.addLink( switchL2_A, host3_A)
+  net.addLink( switchL2_A, host4_A)
+                                            
+  net.addLink( switchL2_B, host1_B)
+  net.addLink( switchL2_B, host2_B)
+  net.addLink( switchL2_B, host3_B)
+  net.addLink( switchL2_B, host4_B)
+                                             
+  net.addLink( switchL2_C, host1_C)
+  net.addLink( switchL2_C, host2_C)
+  net.addLink( switchL2_C, host3_C)
+  net.addLink( switchL2_C, host4_C)
+  
+ 
+  c0=net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633, protocols='OpenFlow13')
+  c1=net.addController('c1', controller=RemoteController, ip='127.0.0.1', port=6653, protocols='OpenFlow13')
   print("***Controller added")
-  
-  
-  net.staticArp()
-  # Add routing for reaching networks that aren't directly connected
- # info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2"))
- # info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth2"))
-  
-  net.start()
+  net.build()
+  switchL2_A.start([c0])
+  switchL2_B.start([c0])
+  switchL2_C.start([c0])
+  switchL3.start([c1])
   time.sleep(1)
   print("*** Running CLI")
   CLI(net)
@@ -97,4 +85,6 @@ def test_RemoteController():
   net.stop()
 
 
-test_RemoteController()
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    topo()
