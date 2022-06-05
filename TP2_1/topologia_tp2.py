b49dcc1d187ead1a1adbc6ed381d13ca912b1626
Maria João Moreira <61200595+mariajmoreira@users.noreply.github.com>
2022-06-05 18:09:32 +0100

    topologia not working :(

--- TP2_1/topologia_tp2.py
+++ TP2_1/topologia_tp2.py
@@ -51,14 +51,14 @@ def topo():
   host3_A = net.addHost('h3A', ip='10.0.1.3/24' ,mac='00:00:00:00:00:03', defaultRoute='via 10.0.1.254')
   host4_A = net.addHost('h4A', ip='10.0.1.4/24' ,mac='00:00:00:00:00:04', defaultRoute='via 10.0.1.254')
   
-  router_B = net.addHost( 'rB', cls=LinuxRouter, mac='00:00:00:00:02:00',ip='10.0.0.2/24' )
+  router_B = net.addHost( 'rB', cls=LinuxRouter, mac='00:00:00:00:02:00',ip='10.0.0.1/24' )
   switchL2_B = net.addSwitch('L2B', protocols='OpenFlow13')
   host1_B = net.addHost('h1B', ip='10.0.2.1/24',mac='00:00:00:00:00:05' , defaultRoute='via 10.0.2.254')
   host2_B= net.addHost('h2B', ip='10.0.2.2/24',mac='00:00:00:00:00:06', defaultRoute='via 10.0.2.254')
   host3_B = net.addHost('h3B', ip='10.0.2.3/24',mac='00:00:00:00:00:07', defaultRoute='via 10.0.2.254')
   host4_B = net.addHost('h4B', ip='10.0.2.4/24',mac='00:00:00:00:00:08', defaultRoute='via 10.0.2.254')
   
-  router_C = net.addHost( 'rC', cls=LinuxRouter, mac='00:00:00:00:03:00' ,ip='10.0.0.3/24')
+  router_C = net.addHost( 'rC', cls=LinuxRouter, mac='00:00:00:00:03:00' ,ip='10.0.0.1/24')
   switchL2_C = net.addSwitch('L2C', protocols='OpenFlow13')
   host1_C = net.addHost('h1C', ip='10.0.3.1/24',mac='00:00:00:00:00:09', defaultRoute='via 10.0.3.254')
   host2_C= net.addHost('h2C', ip='10.0.3.2/24',mac='00:00:00:00:00:10', defaultRoute='via 10.0.3.254')
@@ -67,13 +67,53 @@ def topo():
   
   
   # Add links
-  net.addLink( router_A, router_B)
-  net.addLink( router_B, router_C)
-  net.addLink( router_C, router_A,  delay='5ms')
+  #net.addLink( router_A, router_B)
+  #net.addLink( router_B, router_C)
+  #net.addLink( router_C, router_A,  delay='5ms')
   
-  net.addLink( router_A, switchL2_A)
-  net.addLink( router_B, switchL2_B)
-  net.addLink( router_C, switchL2_C)
+  
+  net.addLink(router_A,
+              router_B,
+              intfName1='rA-eth2',
+              intfName2='rB-eth2',
+              params1={'ip': '10.100.0.1/24'},
+              params2={'ip': '10.100.0.2/24'})
+        
+  net.addLink(router_B,
+              router_C,
+              intfName1='rB-eth3',
+              intfName2='rC-eth2',
+              params1={'ip': '10.100.0.2/24'},
+              params2={'ip': '10.100.0.3/24'})
+
+  net.addLink(router_C,
+              router_A,
+              intfName1='rC-eth3',
+              intfName2='rA-eth3',
+              params1={'ip': '10.100.0.3/24'},
+              params2={'ip': '10.100.0.1/24'},
+              delay='5ms')
+  
+  
+   # Add router-switch links in the same subnet
+  net.addLink(switchL2_A,
+               router_A,
+               intfName2='rA-eth1',
+               params2={'ip': '10.0.0.1/24'})
+  
+  net.addLink(switchL2_B,
+               router_B,
+               intfName2='rB-eth1',
+               params2={'ip': '10.1.0.1/24'})
+  
+  net.addLink(switchL2_C,
+               router_C,
+               intfName2='rC-eth1',
+               params2={'ip': '10.2.0.1/24'})
+  
+  #net.addLink( router_A, switchL2_A)
+  #net.addLink( router_B, switchL2_B)
+  #net.addLink( router_C, switchL2_C)
   
   #net.addLink( switchL3, switchL2_A)
   #net.addLink( switchL3, switchL2_B)
@@ -103,6 +143,10 @@ def topo():
   switchL2_B.start([c0])
   switchL2_C.start([c0])
   switchL3.start([c1])
+  
+  info(net['rA'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev rA-eth2"))
+  info(net['rB'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev rB-eth2"))
+  
   info('*** Routing Table on Router:\n')
   info(net['rA'].cmd('route'))
   info(net['rB'].cmd('route'))
